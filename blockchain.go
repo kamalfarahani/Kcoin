1e416f3fe499fb339602b56e92266174fd13714e
Kamal (Riwogo) <pmer93@gmail.com>
2018-08-14 13:43:22 +0430

    Add blockchain

---
+++ blockchain.go
@@ -0,0 +1,15 @@
+package kcoin
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (blockchain *Blockchain) Add(data string) {
+	prevBlock := blockchain.blocks[len(blockchain.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	blockchain.blocks = append(blockchain.blocks, newBlock)
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
